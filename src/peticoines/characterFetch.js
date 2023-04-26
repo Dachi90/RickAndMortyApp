a5ea766036e26205fa07c6a2520a8f0a3ddddb3e
Martin Bergesio Trabajo <bergesio90@gmail.com>
2023-04-26 14:05:36 +0200

    home

--- src/peticoines/characterFetch.js
+++ src/peticoines/characterFetch.js
@@ -7,7 +7,8 @@ export const characterFetch = async () => {
 		const data = await res.json();
 		console.log(data.results);
 		return data;
-	} catch (res) {
-		console.log(res.statusText);
+	} catch (error) {
+		console.log(error.statusText);
+		return error;
 	}
 };
--- src/presentacion/home/home.js
+++ src/presentacion/home/home.js
@@ -1,5 +1,9 @@
 import html from './home.html?raw';
+import './home.css';
 
 export const Home = () => {
 	return html;
 };
+
+const $characterButton = document.querySelector('#characterButton');
+console.log($characterButton);
